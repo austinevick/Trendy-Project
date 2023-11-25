b1f5f844e187d14d3d52a051dc03d6ff05b28340
austinevick <you@example.com>
2023-11-25 18:53:14 +0100

    modified:   web/package-lock.json 	modified:   web/package.json 	modified:   web/src/App.css 	modified:   web/src/App.tsx 	modified:   web/src/pages/Home.tsx

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,6 +1,22 @@
+import { Clear, Comment } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
+import React from "react"
 
 export const Home = () => {
+    const [isVisible, setIsVisible] = React.useState(false)
+
     return (
-        <div>Home</div>
+        <main className="home">
+            <div className="video">
+                <IconButton onClick={() => setIsVisible(true)}>
+                    <Comment />
+                </IconButton>
+            </div>
+            {isVisible && <div className="comments">
+                <IconButton onClick={() => setIsVisible(false)}>
+                    <Clear />
+                </IconButton>
+            </div>}
+        </main>
     )
 }
